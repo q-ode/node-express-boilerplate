d14baef3cb8c263aa709d35cfebcd32f5e7a1630
q-ode <bayodapo@gmail.com>
2017-01-20 22:20:31 +0100

    Deleting static files and views Removing references to unused files

--- config/app.js
+++ config/app.js
@@ -7,24 +7,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var index = require('./../routes/index');
-var users = require('./../routes/users');
-
 var db = mongoose.connect(process.env.MONGODB_URL);
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes')(app);
 
