9741e22594a173042f981c573a0dac954723d69e
q-ode <bayodapo@gmail.com>
2017-02-08 12:55:28 +0100

    Removed MongoDB URL

--- config/app.js
+++ config/app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var db = mongoose.connect(process.env.MONGODB_URL);
+// var db = mongoose.connect(process.env.MONGODB_URL);
 
 var app = express();
 
